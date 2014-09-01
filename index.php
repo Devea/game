6ea9995dfe2956723da8fc8f38ad298daee871d4
Sparkle <gleamingsparkle@gmail.com>
2014-09-01 17:05:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,36 +3,5 @@
     if(isset($_SESSION["login"])){
     include("php/mysql_connect.php");
     include("php/functions.php");
-	include("header.php");
-    switch(@$_GET['a']) {
-        case 1:
-            include("grain_field.php");
-            include("php/back_icon.php");
-            break;
-        case 2:
-            include("tournament.php");
-            include("php/back_icon.php");
-            break;
-        case 3:
-            include("forest.php");
-            include("php/back_icon.php");
-            break;
-        case 4:
-            include("village.php");
-            include("php/back_icon.php");
-            break;
-        case 5:
-            include("iron_field.php");
-            include("php/back_icon.php");
-            break;
-        default:
-            include("main_map.php");            
     }
-    
-	include("footer.php");
-    
-    }
-    else{
-      header("location: present.php");
-    } 
 ?>
