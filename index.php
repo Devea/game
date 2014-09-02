9d03de3688e9379876f622b60d5310485ab0f22f
Sparkle <gleamingsparkle@gmail.com>
2014-09-02 19:39:46 +0200

    fotky, header v indexu

--- index.php
+++ index.php
@@ -7,39 +7,45 @@
     <meta name="keywords" content="">
     <meta name="author" content="">
     <meta name="robots" content="all">
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 	<script type="text/javascript" src="js/ajax.js"></script>
 </head>
 <body>
-	<div id="login"></div>
-	<script type="text/javascript">
-		var div = document.querySelector("#login");
-		
-		function login(user, pass){
-			post("f=login", "user="+user+"&pass="+pass, function(r){
-				if(r.querySelector("data").innerHTML == "true"){
-					div.innerHTML = "logged";
-				}
-				alert(r.querySelector("message").innerHTML);
-			});
-			return false;
-		}
-		
-		get("f=logged",function(r){
-			if(r.querySelector("logged").innerHTML == "true"){
-				div.innerHTML = "You're logged in!";
-			} else {
-				div.innerHTML = getContent("html/login.html");
-			}
-		});
-	</script>
+    <panel>
+        
+    </panel>
+    <main>
+        
+    </main>
+    <div id="login"></div>
+    <script type="text/javascript">
+            var div = document.querySelector("#login");
+
+            function login(user, pass){
+                    post("f=login", "user="+user+"&pass="+pass, function(r){
+                            if(r.querySelector("data").innerHTML == "true"){
+                                    div.innerHTML = "logged";
+                            }
+                            alert(r.querySelector("message").innerHTML);
+                    });
+                    return false;
+            }
+
+            get("f=logged",function(r){
+                    if(r.querySelector("logged").innerHTML == "true"){
+                            div.innerHTML = "You're logged in!";
+                    } else {
+                            div.innerHTML = getContent("html/login.html");
+                    }
+            });
+    </script>
 </body>
 </html>
 <?php
     /*session_start();
     if(isset($_SESSION["login"])){
-    include("php/mysql_connect.php");
-    include("php/functions.php");
+        include("php/mysql_connect.php");
+        include("php/functions.php");
     }*/
 	
 ?>
